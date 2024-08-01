7fbdbbd6708a23d59779bb84c0ad624281326acf
KaiKoja <126096526+KaiKoja@users.noreply.github.com>
2024-08-01 13:06:04 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,11 @@
+document.querySelector('.menu-icon').addEventListener('click', () => {
+    document.querySelector('.nav-list').classList.toggle('active');
+});
+
+document.querySelector('.prev').addEventListener('click', function() {
+    // Add your previous functionality here
+});
+
+document.querySelector('.next').addEventListener('click', function() {
+    // Add your next functionality here
+});
